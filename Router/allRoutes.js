bdb5d4ad87dc8d133623175c1c1cb6fd4ec8fa67
william77910 <williamfaucomprez@gmail.com>
2025-10-02 14:39:03 +0200

    refactor: Simplification de la syntaxe des routes dans allRoutes.js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,63 +5,54 @@ export const allRoutes = [
   new Route(
     "/galerie",
     "La galerie",
-    "/pages/galerie.html",
-    [],
+    "/pages/galerie.html", [],
     "/js/galerie-admin.js"
   ),
   new Route(
     "/carte",
     "La carte",
-    "/pages/carte.html",
-    [],
+    "/pages/carte.html", [],
     "/js/carte-admin.js"
   ),
   new Route(
     "/signin",
     "Connexion",
-    "/pages/authentification/signin.html",
-    ["disconnected"],
+    "/pages/authentification/signin.html", ["disconnected"],
     "/js/auth/signin.js"
   ),
   new Route(
     "/signup",
     "Inscription",
-    "/pages/authentification/signup.html",
-    ["disconnected"],
+    "/pages/authentification/signup.html", ["disconnected"],
     "/js/auth/signup.js"
   ),
   new Route(
     "/account",
     "Mon compte",
-    "/pages/authentification/account.html",
-    ["client", "admin"],
+    "/pages/authentification/account.html", ["client", "admin"],
     "/js/auth/account.js"
   ),
   new Route(
     "/editPassword",
     "Changement de mot de passe",
-    "/pages/authentification/editPassword.html",
-    ["client", "admin"]
+    "/pages/authentification/editPassword.html", ["client", "admin"]
   ),
   new Route(
     "/allResa",
     "Vos réservations",
-    "/pages/reservations/allResa.html",
-    ["client", "admin"],
+    "/pages/reservations/allResa.html", ["client", "admin"],
     "/js/reservations-manager.js"
   ),
   new Route(
     "/reserver",
     "Réserver",
-    "/pages/reservations/reserver.html",
-    ["client", "admin"],
+    "/pages/reservations/reserver.html", ["client", "admin"],
     "/js/reservation-form.js"
   ),
   new Route(
     "/mentions-legales",
     "Mentions légales",
-    "/pages/mentions-legales.html",
-    []
+    "/pages/mentions-legales.html", []
   ),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
